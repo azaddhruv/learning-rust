42c8e070864e6a61006e35ac39ce77a4b7d708fd
azaddhruv <azaddhruv.com>
2022-11-29 00:41:00 +0530

    learned till match

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,42 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
+// fn main() {
+//     println!("What is your name?");
+//     let mut name = String::new();
+//     let greeting = "Nice to meet you";
+//     io::stdin().read_line(&mut name).expect("Didn't recieve Input"); 
+//     println!("Hello {}! {}", name.trim_end(), greeting);
+// }
+
+// fn main() {
+//     let age = 8;
+//     if(age >= 1) && (age <= 18){
+//         println!("Important Birthday");
+//     } else if (age == 21) && (age == 50){
+//         println!("Important Birthday");
+//     } else {
+//         println!("Not an Important Birthday");
+//     }
+// }
+
+
+// fn main() {
+//     let age2 = 8;
+//     match age2 {
+//         1..=18 => println!("Important Birthday"),
+//         21 | 50 => println!("Important Birthday"),
+//         65..=i32::MAX => println!("Important Birthday"),
+//         _ => println!("Not Important")
+//     };
+// }
+
 fn main() {
-    println!("What is your name?");
-    let mut name = String::new();
-    let greeting = "Nice to meet you";
-    io::stdin().read_line(&mut name).expect("Didn't recieve Input"); 
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You have earn the right to vote"),
+    };
 }
